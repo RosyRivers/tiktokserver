9bdfb01d6b2c1c64435a19788dfb867911f016b3
RosyRivers <1599871125@qq.com>
2023-08-04 22:26:02 +0800

    Initial commit

--- repository/db_init.go
+++ repository/db_init.go
@@ -9,7 +9,7 @@ var db *gorm.DB
 
 func Init() error {
 	var err error
-	dsn := "root:JdecFBmT@tcp(172.16.32.20:49579)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:219969zyt@tcp(localhost:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return err
 }
